531ee5478bee1fcab0d4d2ea979bbd8aa9e8b142
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-08-14 11:02:47 +0300

    Generic Repositories

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product
+    {
+        public int Stock { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }//bire çok ilişki kurduk
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
