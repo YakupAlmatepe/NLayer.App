31a992865eaecb791dfd3f17460ef0a99939b880
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-08-15 18:01:41 +0300

    mvcye giris

---
+++ NLayer.WEB/Services/CategoryApiService.cs
@@ -0,0 +1,23 @@
+﻿using NLayer.Core.DTOs;
+
+namespace NLayer.WEB.Services
+{
+    public class CategoryApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryApiService(HttpClient httpClient)
+        {
+
+
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CategoryDto>> GetAllAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomeResponseDto<List<CategoryDto>>>("categories");
+            return response.Data;
+        }
+
+    }
+}
